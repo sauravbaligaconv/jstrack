97e07d8379a058d42c2dee7e1e3f61e58d98afba
sauravbaligaconv <109660423+sauravbaligaconv@users.noreply.github.com>
2023-01-13 13:09:14 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,6 @@
+
+fetch('http://localhost:3000/')
+    .then(response => response.json())
+    .then(data => {
+        console.log(data);
+    });
---
+++ Backend.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const app = express();
+var cors = require("cors");
+const corsOpts = {
+    origin: '*',
+
+    methods: [
+        'GET',
+        'POST',
+    ],
+
+    allowedHeaders: [
+        'Content-Type',
+    ],
+};
+
+app.use(cors(corsOpts));
+
+
+const IP = require('ip');
+
+app.get('/', (req, res) => {
+    const ipAddress = IP.address();
+    res.json({ ipAddress: ipAddress });
+});
+
+app.listen(3000, () => {
+    console.log('Server listening on port 3000');
+}); 
